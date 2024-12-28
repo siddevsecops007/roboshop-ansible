2c396f1ee8ee1ee78c2deccb30cf346dcfe47103
SathuSid <sisindhriy123@gmail.com>
2024-12-28 21:49:26 +0530

    shipping service changes.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ git pull
 
 if [ $# -ne 2 ]; then
     echo "Input is missing"
-    echo "Usage: run.sh <app component> <env>"
+    echo "Usage: bash run.sh <app component> <env>"
 fi
 
 ansible-playbook -i $2-$1.siddevsecops.icu, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$1 -e app_name=$2 roboshop.yaml
