b589b1dff06a8c4aab5fd2131da7dd9872a350b9
SathuSid <sisindhriy123@gmail.com>
2024-12-28 21:48:54 +0530

    shipping service changes.

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ if [ $# -ne 2 ]; then
     echo "Usage: run.sh <app component> <env>"
 fi
 
-ansible-playbook -i $1-$2.siddevsecops.icu, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$2 -e app_name=$1 roboshop.yaml
+ansible-playbook -i $2-$1.siddevsecops.icu, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$1 -e app_name=$2 roboshop.yaml
