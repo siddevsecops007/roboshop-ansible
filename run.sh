d0010550ca27ade45cbe7495575f5a40c8b34050
SathuSid <sisindhriy123@gmail.com>
2025-01-03 16:35:32 +0530

    vault installation

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ if [ $# -ne 2 ]; then
     echo "Usage: bash run.sh <app component> <env>"
 fi
 
-ansible-playbook -i $2-$1.siddevsecops.icu, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$1 -e app_name=$2 roboshop.yaml
+ansible-playbook -i $2-$1.siddevsecops.icu, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$1 -e component=$2 roboshop.yaml
